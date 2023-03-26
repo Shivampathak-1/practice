29c549005f2e2d3427dced0b91c5afc7c8cbb1bd
Shivam Kumar Pathak <114474974+Shivampathak-1@users.noreply.github.com>
2023-03-26 12:06:24 +0530

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,10 @@
+package practice;
+
+import java.util.Scanner;
+
+public class a2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello world");
+    }
+}
